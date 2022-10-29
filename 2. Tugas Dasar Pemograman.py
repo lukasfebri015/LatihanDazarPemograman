5016b880709f3d98afc188409924c03a3aff8cb2
lukasfebri015 <115157061+lukasfebri015@users.noreply.github.com>
2022-10-29 11:22:00 +0700

    Add files via upload

---
+++ 2. Tugas Dasar Pemograman.py
@@ -0,0 +1,10 @@
+print("Tugas Dasar Pemograman!")
+
+r = int(input("Masukan Rusuknya ="))
+
+luas = 6*r*r
+volume = r*r*r
+
+print("luas Kubus Adalah      = ", luas)
+print("Volume Kubus Adalah     = ", volume)
+ 
