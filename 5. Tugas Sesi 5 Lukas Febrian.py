82932ece39c8aa28293033db73c38bc27f6468ed
lukasfebri015 <115157061+lukasfebri015@users.noreply.github.com>
2022-10-29 11:23:32 +0700

    Add files via upload

---
+++ 5. Tugas Sesi 5 Lukas Febrian.py
@@ -0,0 +1,5 @@
+#Tugas Tentang Input() by Lukas Febrian Laufra - 20220040076
+nama = input ('Nama Sebelumnya')
+namaa = input ('Nama Sesudahnya')
+print('Nama Sebelumnnya', nama)
+print('Nama Sesudahnya:', namaa)
